16867aa54a89fe5bf1fc279d099cb5e63a080cad
Fatma Shaban <fatmashaban@Fatmas-MacBook-Air.local>
2023-07-13 23:40:34 +0300

    edited the setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages, Extension
 import numpy
 
 setup(
-    name='DR16_MassFunction'
-    version="1.0.0"
-    description="calculting the mass-luminosity functions of quasars on DR16 catalog"
-    packages=find_packages()
+    name='DR16_MassFunction',
+    version="1.0.0",
+    description="calculting the mass-luminosity functions of quasars on DR16 catalog",
+    packages=find_packages(),
+    license="LICENSE",
+    incluce_package_data=True,
 
 )
